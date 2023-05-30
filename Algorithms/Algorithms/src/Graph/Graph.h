40dca360f9c7c71047d269846476eabb5cbc04c9
ray <676155220@qq.com>
2023-05-30 20:57:10 +0800

    Update UndirectedMatrixGraph and GridGraph with some algorithms.

---
+++ Algorithms/Algorithms/src/Graph/Graph.h
@@ -0,0 +1,19 @@
+#pragma once
+#pragma once
+
+class Graph {
+public:
+	Graph() : numNodes(0), numEdges(0) {}
+	Graph(int numNodes_, int numEdges_)
+		: numNodes(numNodes_), numEdges(numEdges_) {}
+	Graph(const Graph& rhs) = default;
+	Graph& operator=(const Graph& rhs) = default;
+	Graph(Graph&& rhs) noexcept = default;
+	Graph& operator=(Graph&& rhs) noexcept = default;
+	virtual ~Graph() = default;
+
+	virtual int GetNumNodes() const { return numNodes; }
+	virtual int GetNumEdges() const { return numEdges; }
+protected:
+	int numNodes, numEdges; // 顶点数和边数
+};
